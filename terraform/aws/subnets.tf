bb2f588048ea748ab4b7f92a90565b61c493972b
Bertrand Juglas <bertrand.juglas@ozitem.com>
2018-11-26 18:30:27 +0530

    removing B and C

--- terraform/aws/subnets.tf
+++ terraform/aws/subnets.tf
@@ -12,34 +12,3 @@ resource "aws_subnet" "node-a" {
     Zone         = "public"
   }
 }
-
-# node subnet b
-resource "aws_subnet" "node-b" {
-  vpc_id            ="${ aws_vpc.main.id }"
-  cidr_block        = "${ var.cidr }.5.0/24"
-  availability_zone = "${ var.region }b"
-
-  tags {
-    terraform_id = "${ var.env }-terraform"
-    Name         = "${ var.env }-node-subnet-b"
-    Environment  = "${ var.env }"
-    Role         = "node"
-    Zone         = "public"
-  }
-}
-
-# node subnet c
-resource "aws_subnet" "node-c" {
-  vpc_id            ="${ aws_vpc.main.id }"
-  cidr_block        = "${ var.cidr }.6.0/24"
-  availability_zone = "${ var.region }c"
-
-  tags {
-    terraform_id = "${ var.env }-terraform"
-    Name         = "${ var.env }-node-subnet-c"
-    Environment  = "${ var.env }"
-    Role         = "node"
-    Zone         = "public"
-  }
-}
-
